bb2874640e85e896bf0bd5015f2b7cfd38c5bf5b
Aditya Sharma <aditya.sharma@decimal.co.in>
2020-03-14 21:33:14 +0530

    first commit bootstap add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { NgxChartsModule } from '@swimlane/ngx-charts';
