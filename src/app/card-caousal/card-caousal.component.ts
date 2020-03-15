65cecc4b993fc706d46bfe1fb3e8a924a9e4960f
Himanshu Tamrakar <himanshu.tamrakar@decimal.co.in>
2020-03-15 12:05:07 +0530

    changes

---
+++ src/app/card-caousal/card-caousal.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { DataFetcherService } from '../service/data-fetcher.service';
+
+declare const $: any;
+@Component({
+  selector: 'app-card-caousal',
+  templateUrl: './card-caousal.component.html',
+  styleUrls: ['./card-caousal.component.scss']
+})
+export class CardCaousalComponent implements OnInit {
+
+  first:any;
+  data:any;
+  constructor(private dataFetcherService:DataFetcherService) {
+    this.dataFetcherService.getDataFrom("data/facts/fact.json").subscribe(r => {
+      this.data = r
+    })    
+  }
+
+  ngOnInit() {
+    $('#recipeCarousel').carousel({
+      interval: 10000
+    })
+
+    $('.carousel .carousel-item').each(function () {
+      var minPerSlide = 3;
+      var next = $(this).next();
+      
+      if (!next.length) {
+        next = $(this).siblings(':first');
+      }
+      next.children(':first-child').clone().appendTo($(this));
+
+      for (var i = 0; i < minPerSlide; i++) {
+        next = next.next();
+        if (!next.length) {
+          next = $(this).siblings(':first');
+        }
+
+        next.children(':first-child').clone().appendTo($(this));
+      }
+    });
+
+  }
+
+}
